6a7f7184781c86e44dbe81b0864e5a16772fe36a
Konrad Baran <konradbaran95@gmail.com>
2026-02-07 15:50:47 +0100

    Complete first test of adding nad deleting a board

--- tests/test_first_trello_board.py
+++ tests/test_first_trello_board.py
@@ -1,22 +1,53 @@
+import os
 import requests
+
+from dotenv import load_dotenv
 import pytest
 
 
-# response = requests.get(f"{BASE_URL}/{ENDPOINT}").json()
-# # r = response.json()
-# # print(r)
-# print(response)
+load_dotenv()
+
+BASE_URL = "https://api.trello.com/1/"
+
+API_KEY = os.environ["TRELLO_API_KEY"]
+
+API_TOKEN = os.environ["TRELLO_TOKEN"]
+
+ENDPOINT = "boards/"
 
 
-# def test_create_and_delete_board():
-
-endpoint = "boards/"
-board_name = "python test board 1"
-query_params_post = {
-    "name": board_name,
+def test_create_and_delete_board():
     
-}
+    board_name = "Python test board 07-02-2026 t.2"
 
-create_board_response = requests.post(f"{BASE_URL}/{endpoint}")
-print(create_board_response.status_code)
-    
+    query_params_post = {
+        "name": board_name,
+        "key": API_KEY,
+        "token": API_TOKEN
+    }
+
+    response_post = requests.post(
+        BASE_URL + ENDPOINT,
+        params=query_params_post
+    )
+
+    assert response_post.status_code == 200
+
+    response_post_json = response_post.json()
+
+    assert response_post_json["name"] == board_name
+
+    board_id = response_post_json["id"]
+
+
+    querry_params_delete = {
+        "key": API_KEY,
+        "token": API_TOKEN
+    }
+
+    response_delete = requests.delete(
+        BASE_URL + ENDPOINT + board_id,
+        params=querry_params_delete
+    )
+
+    assert response_delete.status_code == 200
