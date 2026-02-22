648f1510afd216a06d3cec57cf8997b9aaeaed18
Konrad Baran <konradbaran95@gmail.com>
2026-02-22 10:38:58 +0100

    Add negative tests

--- tests/test_boards_with_fixtures.py
+++ tests/test_boards_with_fixtures.py
@@ -33,7 +33,7 @@ def new_board():
 
     board_id = response_post.json()["id"]
 
-    yield board_id
+    yield board_id, board_name
 
     query_params_delete = {
         "key": API_KEY,
@@ -66,40 +66,40 @@ def board_to_delete():
 
 
 def test_get_board_details(new_board):
+    
+    board_id, current_board_name = new_board
 
     query_params_get = {
         "key": API_KEY,
         "token": API_TOKEN,
     }
 
-    board_id = new_board
-
     response_get = requests.get(f"{BASE_URL}boards/{board_id}", params=query_params_get)
 
     assert response_get.status_code == 200
 
-    assert response_get.json()["name"] == "fixture test board 1.00"
+    assert response_get.json()["name"] == current_board_name
 
 
 def test_update_board(new_board):
     
-    board_id = new_board
+    board_id, _ = new_board
 
-    board_name = "testing board updated"
+    new_board_name = "testing board updated"
 
     querry_params_put = {
         "key": API_KEY,
         "token": API_TOKEN,
-        "name": board_name
+        "name": new_board_name
     }
 
     response_put = requests.put(f"{BASE_URL}boards/{board_id}", params=querry_params_put)
 
-    response_put_json = response_put.json()
+    # response_put_json = response_put.json()
 
     assert response_put.status_code == 200
 
-    assert response_put_json["name"] == board_name
+    assert response_put.json()["name"] == new_board_name
 
 
 def test_delete_board(board_to_delete):
@@ -116,4 +116,40 @@ def test_delete_board(board_to_delete):
 
     assert response_del.status_code == 200
 
+
+# NEGATIVE TESTS:
+def test_get_non_existent_board_returns_404():
+    
+    non_existent_id = "010101010101010101010101"
+
+    url = f"{BASE_URL}boards/{non_existent_id}"
+
+    query_params_get = {
+        "key": API_KEY,
+        "token": API_TOKEN
+    }
+
+    response_get = requests.get(url, params=query_params_get)
+
+    assert response_get.status_code == 404
+
+
+def test_create_board_wrong_token():
+
+    board_name = "test board 1.00"
+
+    url = f"{BASE_URL}boards/"
+
+    query_params_post = {
+        "key": API_KEY,
+        "token": "ATTAf00e0f00f00b000c00c00000be00d00ccf00a0000cb00000eb0000000b0e000bC00AF00E",
+        "name": board_name
+    }
+
+    response_post = requests.post(url, params=query_params_post)
+
+    assert response_post.status_code == 401
+
+
+# $ pytest test_boards_with_fixtures.py::get_non_existent_board_returns_404 -v
 # $ pytest test_boards_with_fixtures.py -v -s
