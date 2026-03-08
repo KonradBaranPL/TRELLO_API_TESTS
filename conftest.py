737f4f0c56accf73f1f3b69fd7b5ed40c0705603
Konrad Baran <konradbaran95@gmail.com>
2026-03-08 21:24:54 +0100

    refactor (fixtures): add missing dockstringd

--- conftest.py
+++ conftest.py
@@ -1,61 +1,45 @@
 import requests
+import uuid
 
 from config import API_KEY, API_TOKEN, BASE_URL
 import pytest
 
 
 @pytest.fixture
-def auth():
+def auth_params():
+    """Return Trello API key and token for authenticated requests."""
     return {"key": API_KEY, "token": API_TOKEN}
 
 
 @pytest.fixture
-def new_board():
+def new_board(auth_params):
     """Creates a new board and deletes it at the end of the test. Returns board id and board name"""
-    
-    board_name = "fixture test board 1.0"
 
-    query_params_post = {
-        "name": board_name,
-        "key": API_KEY,
-        "token": API_TOKEN,
-        "defaultLists": "false"
-    }
+    unique_id = uuid.uuid4[:6]
+    board_name = f"test_board_{unique_id}"
+    url = f"{BASE_URL}board/"
+    query_params_post = {**auth_params, "name": board_name}
 
-    response_post = requests.post(f"{BASE_URL}boards/", params=query_params_post)
-
+    response_post = requests.post(url, params=query_params_post)
     assert response_post.status_code == 200
-
     board_id = response_post.json()["id"]
-
     yield board_id, board_name
 
-    query_params_delete = {
-        "key": API_KEY,
-        "token": API_TOKEN
-    }
-
-    response_del = requests.delete(f"{BASE_URL}boards/{board_id}", params=query_params_delete)
-
+    query_params_delete = {**auth_params}
+    response_del = requests.delete(f"{url}{board_id}", params=query_params_delete)
     assert response_del.status_code == 200
 
 
 @pytest.fixture
-def board_to_delete():
+def board_to_delete(auth_params):
     """Creates a board without deleting it at the end of the test. Returns board_id"""
 
-    board_name = "board XYZ"
+    unique_id = uuid.uuid4[:6]
+    board_name = f"test_board_{unique_id}"
+    url = f"{BASE_URL}boards/"
+    query_params = {**auth_params, "name": board_name}
 
-    query_params_post = {
-        "key": API_KEY,
-        "token": API_TOKEN,
-        "name": board_name
-    }
-
-    response_post = requests.post(f"{BASE_URL}boards/", params=query_params_post)
-    
+    response_post = requests.post(url, params=query_params)
     assert response_post.status_code == 200
-
     board_id = response_post.json()["id"]
-
     yield board_id
