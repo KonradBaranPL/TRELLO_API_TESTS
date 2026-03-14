c723c7f8f6feb1ea4b053b00b3fd3e3ecbc86d64
Konrad Baran <konradbaran95@gmail.com>
2026-03-14 10:08:20 +0100

    feat (class): implement update_board method in TrelloClient

--- helpers/trello_client.py
+++ helpers/trello_client.py
@@ -1,23 +1,51 @@
 import requests
+import uuid
 
 from config import API_KEY, API_TOKEN, BASE_URL
 
 
-class ApiClient:
+class TrelloClient:
+    "Client for interacting with the Trello API"
+
     def __init__(self):
+        """Initialize API client with authentication credentials."""
+
         self.key = API_KEY
         self.token = API_TOKEN
         self.base_url = BASE_URL
     
 
     def create_board(self, name):
+        """Create a new Trello board"""
+
         params = {"name": name, "key": self.key,"token": self.token}
         return requests.post(f"{self.base_url}boards/", params=params)
     
+
     def get_board(self, board_id):
+        """Retrieve information about a specific Trello board"""
+
         params = {"key": self.key,"token": self.token}
         return requests.get(f"{self.base_url}boards/{board_id}", params=params)
     
+
+    def update_board(self, board_id):
+        """Update a Trello board"""
+
+        params = {"key": self.key, "token": self.token}
+        return requests.put(f"{self.base_url}boards/{board_id}", params=params)
+
+
     def delete_board(self, board_id):
+        """Delete a Trello board"""
+
         params = {"key": self.key,"token": self.token}
         return requests.delete(f"{self.base_url}boards/{board_id}", params=params)
+    
+
+    def unique_board_name(self):
+        """Create board name ending with 8-character unique identifier"""
+
+        unique_id = str(uuid.uuid4())[:8]
+        board_name = f"board_{unique_id}"
+        return board_name
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,47 +1,32 @@
-import requests
-import uuid
-from config import API_KEY, API_TOKEN, BASE_URL
-from typing import Generator
+from helpers.trello_client import TrelloClient
 
 import pytest
 
 
 @pytest.fixture
-def auth_params() -> dict[str, str]:
-    """Return Trello API key and token for authenticated requests."""
-    return {"key": API_KEY, "token": API_TOKEN}
+def trello_client():
+    """Returns an object of TrelloClient class"""
+    return TrelloClient()
 
 
 @pytest.fixture
-def new_board(auth_params: dict[str, str]) -> Generator[dict[str, str], None, None]:
-    """Creates a new board and deletes it at the end of the test. Returns board id and board name"""
+def new_board(trello_client):
+    """Creates a new board and deletes it at the end of the test. Returns board id"""
+    board_name = f"fixture_{trello_client.unique_board_name()}"
+    response = trello_client.create_board(board_name)
+    board_id = response.json()["id"]
+    yield board_id
 
-    unique_id = str(uuid.uuid4())[:6]
-    unique_board_name = f"test_board_{unique_id}"
-    url = f"{BASE_URL}board/"
-    query_params_post = {**auth_params, "name": unique_board_name}
-
-    response_post = requests.post(url, params=query_params_post)
-    assert response_post.status_code == 200
-    board_id = response_post.json()["id"]
-    board_name = response_post.json()["name"]
-    yield {"id": board_id, "name": board_name}
-
-    query_params_delete = {**auth_params}
-    response_del = requests.delete(f"{url}{board_id}", params=query_params_delete)
-    assert response_del.status_code == 200
+    response = trello_client.delete_board(board_id)
+    assert response.status_code == 200
 
 
 @pytest.fixture
-def board_to_delete(auth_params: dict[str, str]) -> Generator[str, None, None]:
+def board_to_delete(trello_client):
     """Creates a board without deleting it at the end of the test. Returns board_id"""
 
-    unique_id = uuid.uuid4[:6]
-    board_name = f"test_board_{unique_id}"
-    url = f"{BASE_URL}boards/"
-    query_params = {**auth_params, "name": board_name}
-
-    response_post = requests.post(url, params=query_params)
-    assert response_post.status_code == 200
-    board_id = response_post.json()["id"]
+    board_name = f"fixture_{trello_client.unique_board_name()}"
+    response = trello_client.create_board(board_name)
+    assert response.status_code == 200
+    board_id = response.json()["id"]
     yield board_id
